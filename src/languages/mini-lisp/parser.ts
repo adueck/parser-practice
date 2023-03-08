3b2abf8ec77c9b1624d4a9f17579269cc759b54a
adueck <adueck@users.noreply.github.com>
2023-03-08 16:51:12 +0500

    added purely functional mini-lisp parser

--- src/languages/mini-lisp/parser.ts
+++ src/languages/mini-lisp/parser.ts
@@ -9,7 +9,7 @@ import {
     SL,
 } from "./types";
 
-export function parseMiniLisp(tokens: Readonly<(string|number)[]>): SExp {
+function parseMiniLisp(tokens: Readonly<(string|number)[]>): SExp {
     const t = [...tokens];
     return parseSExp();
     function parseSExp(): SExp {
@@ -34,9 +34,60 @@ export function parseMiniLisp(tokens: Readonly<(string|number)[]>): SExp {
     }
 }
 
+function parseMiniLispF(tokens: Readonly<(string|number)[]>): SExp {
+    const { sexp, tokens: leftovers } = parseSExp(tokens);
+    if (leftovers.length) {
+        throw new Error("trailing tokens");
+    }
+    return sexp;
+}
+
+function parseSExp(tokens: Readonly<(string|number)[]>): {
+    sexp: SExp,
+    tokens: (string|number)[],
+} {
+    const [first, ...rest] = tokens;
+    if (typeof first === "number") {
+        return {
+            sexp: first,
+            tokens: rest,
+        };
+    } else {
+        const { sl, tokens: leftovers } = parseSL(tokens);
+        return {
+            sexp: sl,
+            tokens: leftovers,
+        }
+    }
+}
+function parseSL(tokens: Readonly<(string|number)[]>): {
+    sl: SL,
+    tokens: (string|number)[],
+} {
+    const [first, f, ...rest] = tokens;
+    if (first !== "(") throw new Error("expected (");
+    const { sexp: s1, tokens: l1 } = parseSExp(rest);
+    const { sexp: s2, tokens: l2 } = parseSExp(l1);
+    const sl: SL = [
+        f as FExp,
+        s1,
+        s2,
+    ];
+    const [closing, ...rest2] = l2;
+    if (closing !== ")") throw new Error("expected )");
+    return {
+        sl,
+        tokens: rest2,
+    };
+}
+
 export default [
     {
         parser: parseMiniLisp,
         label: "parseMiniLisp",
+    },
+    {
+        parser: parseMiniLispF,
+        label: "parseMiniLispF",
     },
 ];
