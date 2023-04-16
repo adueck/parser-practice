e634c5372fbc1b39859d0ed8d98cb7ef158716c6
adueck <adueck@users.noreply.github.com>
2023-04-15 21:54:05 -0400

    update

--- src/languages/mini-lisp/grammar.ts
+++ src/languages/mini-lisp/grammar.ts
@@ -9,6 +9,7 @@ Semantic rules:
  - The first SE in a SL must be a function or reference to a function
  - inbuilt functions are + - * / = < >
  - LISP like
+   - if: (if SE SE SE)
    - variable defs: (let (string SE) SE)
    - lambda functions: (lambda string SE)
 `;
